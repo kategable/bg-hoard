79263f58e9e4644b99740a812159fe254d8993d1
Kate Sky <kateskytech@gmail.com>
2021-09-14 10:49:23 -0500

    labs 1-2

---
+++ apps/store/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { getAllGames } from '../fake-api';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bg-hoard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Board Game Hoard';
+  games = getAllGames();
+}
